99d96112e7cdf8275a1d68df08ac3fdfe29f6468
MassinissaDjellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-04-12 13:03:01 -0400

    Remplacement des queries dans DocumentRepository par findAllBy

--- src/main/java/com/repository/DocumentRepository.java
+++ src/main/java/com/repository/DocumentRepository.java
@@ -7,16 +7,18 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
+import javax.print.Doc;
 import java.util.List;
 import java.util.Optional;
 @Repository
 public interface DocumentRepository extends JpaRepository<Documents,Long> {
-    @Query("select l from Livre l where l.titre = :titre")
-    public Optional<List<Livre>> findBooksByTitre(String titre);
-    @Query("select m from Media m where m.titre = :titre")
-    public Optional<List<Media>> findMediaByTitre(String titre);
-    @Query("select l from Livre l where l.titre = :auteur")
-    public Optional<List<Livre>> findBooksByAuteur(String auteur);
-    @Query("select m from Media m where m.titre = :auteur")
-    public Optional<List<Media>> findMediaByAuteur(String auteur);
+    public Optional<List<Documents>> findAllByTitreContaining(String titre);
+    public Optional<List<Documents>> findAllByAuteur(String auteur);
+
+//    @Query("select m from Media m where m.titre = :titre")
+//    public Optional<List<Media>> findMediaByTitre(String titre);
+//    @Query("select l from Livre l where l.titre = :auteur")
+//    public Optional<List<Livre>> findBooksByAuteur(String auteur);
+//    @Query("select m from Media m where m.titre = :auteur")
+//    public Optional<List<Media>> findMediaByAuteur(String auteur);
 }
