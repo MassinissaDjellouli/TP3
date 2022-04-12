f36863fd5b3687e9ee58b563f685e9afcc6ac2a9
MassinissaDjellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-04-12 12:11:02 -0400

    Ajout des query pour findBook et findMedia ByTitle

--- src/main/java/com/repository/DocumentRepository.java
+++ src/main/java/com/repository/DocumentRepository.java
@@ -1,7 +1,18 @@
 package com.repository;
 
 import com.models.documents.Documents;
+import com.models.documents.Livre;
+import com.models.documents.Media;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+@Repository
 public interface DocumentRepository extends JpaRepository<Documents,Long> {
+    @Query("select l from Livre l where l.titre = :titre")
+    public Optional<List<Livre>> findBooksByTitle(String titre);
+    @Query("select m from Media m where m.titre = :titre")
+    public Optional<List<Media>> findMediaByTitle(String titre);
 }
