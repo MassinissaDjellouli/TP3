0656d2890581f5a4f4e723b80d75c5f7a45e6bc5
MassinissaDjellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-04-12 11:10:27 -0400

    Creation des fonctions statiques dans ModelToDTOTransformer

--- src/main/java/com/dto/ModelToDTOTransformer.java
+++ src/main/java/com/dto/ModelToDTOTransformer.java
@@ -1,5 +1,24 @@
 package com.dto;
 
-public class ModelToDTOTransformer {
+import com.models.Dette;
+import com.models.Emprunt;
+import com.models.documents.Livre;
+import com.models.documents.Media;
 
+public class ModelToDTOTransformer {
+    public static EmpruntDTO empruntToDTO(Emprunt emprunt){
+        return null;
+    }
+    public static DetteDTO detteToDTO(Dette dette){
+        return null;
+    }
+    public static DateDTO dateEmpruntToDTO(Emprunt emprunt){
+        return null;
+    }
+    public static LivreDTO LivreToDTO(Livre livre){
+        return null;
+    }
+    public static MediaDTO mediaToDTO(Media media){
+        return null;
+    }
 }
