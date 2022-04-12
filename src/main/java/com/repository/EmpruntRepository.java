f6958c204e9b235b4de04e1658114d314122f62c
MassinissaDjellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-04-12 12:05:58 -0400

    Ajout de @Repository dans les repo

--- src/main/java/com/repository/EmpruntRepository.java
+++ src/main/java/com/repository/EmpruntRepository.java
@@ -2,6 +2,8 @@ package com.repository;
 
 import com.models.Emprunt;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface EmpruntRepository extends JpaRepository<Emprunt,Long> {
 }
