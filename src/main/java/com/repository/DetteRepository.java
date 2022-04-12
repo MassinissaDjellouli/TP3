db35dbca33d2df5081087b84680dd749cf7f5bb6
MassinissaDjellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-04-12 11:01:09 -0400

    Creation de DetteRepository

---
+++ src/main/java/com/repository/DetteRepository.java
@@ -0,0 +1,7 @@
+package com.repository;
+
+import com.models.Dette;
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface DetteRepository extends JpaRepository<Dette,Long> {
+}
