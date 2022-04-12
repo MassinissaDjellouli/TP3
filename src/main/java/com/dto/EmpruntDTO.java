e71c4cc691d0714b44ab6911ad78e880bda5b28d
MassinissaDjellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-04-12 10:49:01 -0400

    Implémentation de EmpruntDTO

--- src/main/java/com/dto/EmpruntDTO.java
+++ src/main/java/com/dto/EmpruntDTO.java
@@ -1,4 +1,12 @@
 package com.dto;
 
+import lombok.Data;
+
+import java.time.LocalDateTime;
+@Data
 public class EmpruntDTO {
+    private String clientName;
+    private String empruntDate;
+    private String returnDate;
+    private String documentName;
 }
