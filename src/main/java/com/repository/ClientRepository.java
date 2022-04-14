a9ad8941c56dd12b207ad227f1f994504465acae
MassinissaDjellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-04-14 12:57:16 -0400

    Ajout d'une query pour trouver les clients avec leurs emprunts

--- src/main/java/com/repository/ClientRepository.java
+++ src/main/java/com/repository/ClientRepository.java
@@ -2,8 +2,13 @@ package com.repository;
 
 import com.models.users.Client;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
 
 @Repository
 public interface ClientRepository extends JpaRepository<Client,Integer> {
+    @Query("select c from Client c left join fetch c.emprunts")
+    public Optional<Client> findByIdWithEmprunts(Integer integer);
 }
