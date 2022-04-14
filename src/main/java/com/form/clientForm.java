1ba27c8d0d0d461674bbb5845028c11dc5bf1353
MassinissaDjellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-04-14 16:32:23 -0400

    Implémentation de clientForm

--- src/main/java/com/form/clientForm.java
+++ src/main/java/com/form/clientForm.java
@@ -1,4 +1,12 @@
 package com.form;
 
+import lombok.Builder;
+import lombok.Data;
+
+@Data
+@Builder
 public class clientForm {
+    public String name;
+    public String adress;
+    public String phone;
 }
