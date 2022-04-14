5478b1e2fb607f054af68c4c0ea9f29c5c9718da
MassinissaDjellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-04-14 14:18:23 -0400

    Ajout d'un DateTimeFormatter dans DateDTO

--- src/main/java/com/dto/DateDTO.java
+++ src/main/java/com/dto/DateDTO.java
@@ -3,10 +3,16 @@ package com.dto;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import org.springframework.format.datetime.standard.DateTimeFormatterFactory;
+
+import java.time.DateTimeException;
+import java.time.format.DateTimeFormatter;
+import java.time.format.DateTimeFormatterBuilder;
 
 @Data
 @Builder
 public class DateDTO {
+    public static final DateTimeFormatter DATE_TIME_FORMATTER = DateTimeFormatter.ofPattern("dd/MM/yyyy");
     private String clientName;
     private String documentName;
     private String dateEmprunt;
