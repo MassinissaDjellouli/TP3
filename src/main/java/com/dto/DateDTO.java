08612213728a5d60cdf549a3dcde916b82f534d6
MassinissaDjellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-04-14 14:09:07 -0400

    ajout de @Builder dans dateDTO

--- src/main/java/com/dto/DateDTO.java
+++ src/main/java/com/dto/DateDTO.java
@@ -1,10 +1,11 @@
 package com.dto;
 
 import lombok.AllArgsConstructor;
+import lombok.Builder;
 import lombok.Data;
 
 @Data
-@AllArgsConstructor
+@Builder
 public class DateDTO {
     private String clientName;
     private String documentName;
