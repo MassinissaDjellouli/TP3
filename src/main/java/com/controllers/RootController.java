664f47fb8ece49f7be72b58b77e0d7462648769d
MassinissaDjellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-04-14 15:31:24 -0400

    Creation du RootController

---
+++ src/main/java/com/controllers/RootController.java
@@ -0,0 +1,11 @@
+package com.controllers;
+
+import org.slf4j.LoggerFactory;
+
+import org.slf4j.Logger;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+public class RootController {
+
+}
