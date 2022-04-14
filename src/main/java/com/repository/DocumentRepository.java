06cbb15ee1e4e8a5e853c9b23953c0528225f0c9
MassinissaDjellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-04-13 21:36:27 -0400

    Ajout de @Param dans les fonctions

--- src/main/java/com/repository/DocumentRepository.java
+++ src/main/java/com/repository/DocumentRepository.java
@@ -6,6 +6,7 @@ import com.models.documents.Media;
 import com.models.enums.Genres;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import javax.print.Doc;
@@ -13,11 +14,11 @@ import java.util.List;
 import java.util.Optional;
 @Repository
 public interface DocumentRepository extends JpaRepository<Documents,Integer> {
-    public Optional<List<Documents>> findAllByTitreContaining(String titre);
-    public Optional<List<Documents>> findAllByAuteur(String auteur);
-    public Optional<List<Documents>> findAllByAnneeDePublication(int anne);
+    public Optional<List<Documents>> findAllByTitreContaining(@Param("titre") String titre);
+    public Optional<List<Documents>> findAllByAuteur(@Param("auteur") String auteur);
+    public Optional<List<Documents>> findAllByAnneeDePublication(@Param("anne") int anne);
     @Query("select l from Livre l where l.genre = :genre")
-    public Optional<List<Documents>> findAllByGenre(Genres genre);
+    public Optional<List<Documents>> findAllByGenre(@Param("genre") Genres genre);
 
 //    @Query("select m from Media m where m.titre = :titre")
 //    public Optional<List<Media>> findMediaByTitre(String titre);
