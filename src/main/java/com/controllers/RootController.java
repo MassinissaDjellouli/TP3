de23dca2f898104fba5e56e572af42cbb3446683
MassinissaDjellouli <79031818+MassinissaDjellouli@users.noreply.github.com>
2022-04-14 15:47:30 -0400

    Creation des routes dans index.html et RootController

--- src/main/java/com/controllers/RootController.java
+++ src/main/java/com/controllers/RootController.java
@@ -1,11 +1,21 @@
 package com.controllers;
 
-import org.slf4j.LoggerFactory;
-
-import org.slf4j.Logger;
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 
+@Controller
 public class RootController {
-
+    @GetMapping("/")
+    public String getIndex(Model model){
+        return "index";
+    }
+    @GetMapping("/users")
+    public String getUsers(Model model){
+        return "users";
+    }
+    @GetMapping("/employees")
+    public String getEmployes(Model model){
+        return "employees";
+    }
 }
